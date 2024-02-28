d5ef54bea9c5900c2956091c918e6da4c3465f1e
Kamil Gareev <144125755+kamilgareev@users.noreply.github.com>
2024-02-29 02:11:48 +0700

    minor update

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -7,7 +7,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG_STATUS')
+
+#DEBUG = os.getenv('DEBUG_STATUS')
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
