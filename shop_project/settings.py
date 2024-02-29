81184f6164c8452d06ed74d68930f8d599d770e0
Kamil Gareev <144125755+kamilgareev@users.noreply.github.com>
2024-02-29 10:35:20 +0700

    updage debug status

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG_STATUS')
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
